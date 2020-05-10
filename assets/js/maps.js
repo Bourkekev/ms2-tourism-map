215891a27cdd3af58ca756b5d25d16cec99352c2
Kevin <bourkekev@gmail.com>
2020-05-10 17:27:57 +0100

    Add basic footer structure and add single custom icon marker to the map with an Info window

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,4 +9,20 @@ function initMap() {
     }
     let map = new google.maps.Map(document.getElementById("map"), options);
 
+    /* SINGLE MAP MARKER */
+    let marker = new google.maps.Marker({
+        position:  { lat: 53.4246, lng: -6.1210 },
+        map: map,
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+    });
+
+    //info window
+    let infoWindow = new google.maps.InfoWindow({
+        content: '<h2>Velvet Strand</h2><p>Portmarnock</p><h3>Hi!</h3>'
+    });
+
+    // need to add listener to listen for that info window
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker)
+    });
 }
