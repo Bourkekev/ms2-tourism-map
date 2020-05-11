a160116bfe4c7870706281edde1c170b549e30c1
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-11 18:57:37 +0100

    Put all addMarkers called into array instead of individual addMarker calls and loop through markers to call addMarker function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -27,19 +27,52 @@ function initMap() {
         infoWindow.open(map, marker)
     });
     */
-    addMarker({
-        coords:{lat: 53.4246, lng: -6.1210 },
-        iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        content: '<h2>Velvet Strand</h2><p>Portmarnock</p><h3>Hi!</h3>'
-    });
-    addMarker({
-        coords:{lat: 53.4509, lng: -6.1501 },
-        iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    });
-    addMarker({
-        coords:{lat: 53.3786, lng: -6.0570 },
+
+    // Put infoWindow content into variable
+    let portoContent = `
+        <h2>Velvet Strand</h2>
+        <p>Portmarnock</p>
+        <p><a href="https://www.visitdublin.com/see-do/details/portmarnock-the-velvet-strand-blue-flag-beach-2019" target="_blank">See more here</a></p>`
+    // Put all addMarkers called into array instead of individual addMarker calls
+    let markers = [
+        {
+            coords:{lat: 53.4246, lng: -6.1210 },
+            iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+            content: portoContent
+        },
+        {
+            coords:{lat: 53.4509, lng: -6.1501 },
+            iconImage: "http://maps.google.com/mapfiles/kml/pal2/icon27.png",
+            content: '<h2>Party Village</h2><p>Malahide</p>'
+        },
+        {
+            coords:{lat: 53.3786, lng: -6.0570 },
+            iconImage: "http://maps.google.com/mapfiles/ms/micons/fishing.png",
+            content: '<h2>Fishing Village</h2><p>Howth</p>'
+        }
+    ];
+
+    //loop through markers
+    for(let i = 0; i < markers.length; i++){
+        addMarker(markers[i]);
+    }
+
+
+    // addMarker({
+    //     coords:{lat: 53.4246, lng: -6.1210 },
+    //     iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    //     content: '<h2>Velvet Strand</h2><p>Portmarnock</p><h3>Hi!</h3>'
+    // });
+    // addMarker({
+    //     coords:{lat: 53.4509, lng: -6.1501 },
+    //     iconImage: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    //     content: '<h2>Party Village</h2><p>Malahide</p>'
+    // });
+    // addMarker({
+    //     coords:{lat: 53.3786, lng: -6.0570 },
+    //     content: '<h2>Fishing Village</h2><p>Howth</p>'
         
-    });
+    // });
 
     // Add Marker function
     function addMarker(props){
