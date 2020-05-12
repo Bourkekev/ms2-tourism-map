fe8e515e3406125ab1b518f4ff4edf956f9d569a
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-12 08:19:51 +0100

    Get input from field on button click

--- assets/js/takeNotes.js
+++ assets/js/takeNotes.js
@@ -1,2 +1,9 @@
-let submitButton = getElementById("submit-note");
-let userInput = getElementById("note-input");
+let submitButton = document.getElementById("submit-note");
+let userInput = document.getElementById("note-input");
+let list = document.getElementById("notes-list");
+
+function addToLIst(){
+    let newItemValue = document.getElementById("note-input").value;
+    console.log("add to list initiated");
+    document.getElementById("notes-list").innerHTML = "<p>Item is " + newItemValue + "</p>";
+}
