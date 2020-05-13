9a41d38fedf1fcb4b7edc954da9854ed50885d6d
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-13 20:39:55 +0100

    Get sendEmail script to get local storage in variable and send this variable in place of form text area

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,17 @@
 function sendMail(notesForm){
+    //get local stored list
+    //let currentList = localStorage.getItem("listItem");
+    let sendList = "<ul>";
+    console.log(sendList + "currentlist" + currentList);
+    if (currentList){
+        sendList += currentList;
+        sendList += "<ul>";
+    }
+    console.log(sendList);
     let templateParams = {
         "from_email": notesForm.email.value,
-        "notes_summary": notesForm.notessummary.value
+        //"notes_summary": notesForm.notessummary.value
+        "notes_summary": sendList
     };
      
     emailjs.send('gmail', 'template_tourism_notes', templateParams)
