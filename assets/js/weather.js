64272b82551e5f118a7ace7c51e75ff227ed1eb2
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-14 09:18:40 +0100

    Get JSON data with button, create fucntion to write to doc but remove type parameter passed in as was used for Star Wars api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,22 +1,48 @@
-  
-let xhr = new XMLHttpRequest();
-// This number is Dublin ID
 let city = 2072525;
-let apiKey = "4adecede8cc646766529fd7932ba0555";
-let weatherData;
+const apiKey = "4adecede8cc646766529fd7932ba0555";
+const baseURL = `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`;
 
-xhr.open("GET", `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}`);
-xhr.send();
+// function getData(type, callBack) {
+//     let xhr = new XMLHttpRequest();
+    
+//     xhr.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             callBack(JSON.parse(this.responseText));
+//         }
+//     };
 
-// use this function to get the data out of onreadystate
-function setWeatherData(jsonData) {
-    weatherData = jsonData;
-    console.log(weatherData);
-    document.getElementById("weather-data").innerHTML = weatherData;
+//     //xhr.open("GET", baseURL + type + "/");
+//     xhr.open("GET", baseURL + "/");
+//     xhr.send();
+// }
+
+// without type
+function getData(callBack) {
+    let xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callBack(JSON.parse(this.responseText));
+        }
+    };
+
+    //xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", baseURL);
+    xhr.send();
 }
-//onreadystatechange
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        setWeatherData(JSON.parse(this.responseText));
-    }
-};
+
+// function writeToDocument() {
+//     getData(type, function(data) {
+//         document.getElementById("weather-data").innerHTML = data;
+//     });
+// }
+
+// without type
+function writeToDocument() {
+    getData( function(data) {
+        document.getElementById("weather-data").innerHTML = data;
+    });
+}
+
+
+//console.log(weatherData);
