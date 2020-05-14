76a13e9d993a4cb5fa011bfe9ed1e88382ab7e93
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-14 14:06:17 +0100

    Break weather api further for City, Country. Get temp, city and country from API

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,22 +1,10 @@
-let city = 2072525;
+//let city = 2964574;
+let city = "Dublin";
+let countryCode = "IE";
 const apiKey = "4adecede8cc646766529fd7932ba0555";
 let tempUnits = "units=metric";
-const baseURL = `http://api.openweathermap.org/data/2.5/weather?id=${city}&${tempUnits}&appid=${apiKey}`;
-
-
-// function getData(type, callBack) {
-//     let xhr = new XMLHttpRequest();
-    
-//     xhr.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             callBack(JSON.parse(this.responseText));
-//         }
-//     };
-
-//     //xhr.open("GET", baseURL + type + "/");
-//     xhr.open("GET", baseURL + "/");
-//     xhr.send();
-// }
+const baseURLByID = `http://api.openweathermap.org/data/2.5/weather?id=${city}&${tempUnits}&appid=${apiKey}`;
+const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&${tempUnits}&appid=${apiKey}`;
 
 // without type
 function getData(callBack) {
@@ -33,19 +21,19 @@ function getData(callBack) {
     xhr.send();
 }
 
-// function writeToDocument() {
-//     getData(type, function(data) {
-//         document.getElementById("weather-data").innerHTML = data;
-//     });
-// }
-
 // without type
 function writeToDocument() {
     getData( function(data) {
-        document.getElementById("weather-data").innerHTML = "Temperature is: " + data.main.temp;
-        console.log(data.main.temp);
+
+        let weather = data.weather;
+        weather.forEach(function(item){
+            //document.getElementById("weather-data").innerHTML = "Weather for " + city + item.sys.country + ": " + item.main;
+
+        });
+        document.getElementById("weather-data").innerHTML = "<p>Weather for " + data.name + ", " + data.sys.country + ": " + data.weather[0].main + "</p>";
+        document.getElementById("weather-data").innerHTML += "<p>Temperature is: " + data.main.temp + " Celcius</p>";
+        
+        //document.getElementById("weather-data").innerHTML = "Temperature is: " + data.main.temp;
+        console.dir(data);
     });
 }
-
-
-//console.log(weatherData);
