535337b61241feee5512c8e2ab782ff1276f216d
Kevin Bourke <kevinbourke@Kevins-iMac.local>
2020-05-21 11:32:18 +0100

    Trying to remove button from emailed notes list items. Replace removes only first item

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,10 +1,15 @@
 function sendMail(notesForm){
     //get local stored list
     currentList = localStorage.getItem("listItem");
+    console.log(typeof(currentList)+currentList);
     let sendList = "<ul>";
     console.log( "Current LIST: " + sendList + currentList);
+    
+
     if (currentList){
-        sendList += currentList;
+        //remove buttons
+        let currentListNoBtn = currentList.replace(/<button class="del-btn">X</button>/g, 'hel');
+        sendList += currentListNoBtn;
         sendList += "<ul>";
     }
     console.log("LIST TO BE SENT: " + sendList);
@@ -13,11 +18,12 @@ function sendMail(notesForm){
         "notes_summary": sendList
     };
     
-    // debug
+    // debug : send list back to DOM
     let debugEl = document.getElementById("note-send-debug");
     if (sendList){
         debugEl.innerHTML = sendList;
     }
+
     // emailjs.send('gmail', 'template_tourism_notes', templateParams)
     //     .then(function(response) {
     //        console.log('SUCCESS!', response.status, response.text);
