22884cec03855cce4c8441238bd196cc90363365
Kevin <bourkekev@gmail.com>
2020-05-23 16:45:52 +0100

    get google places searchbox working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -400,7 +400,7 @@ let markers = [];
 function initMap() {
     //map options
     let options = {
-        zoom: 11,
+        zoom: 14,
         center: {
             lat: 53.347293,
             lng: -6.25897,
@@ -599,9 +599,87 @@ function initMap() {
     });
     // To use a POLYGON just use .Polygon in above example
     // call function to add line
-    addLine();
+    addPoly();
 
-}
+    // Add a listener for the click event.
+    shoppingAreaNorth.addListener('click', showArrays);
+
+    infoWindow = new google.maps.InfoWindow({maxWidth: 200});
+
+    /**
+ * Google Places
+ */
+// Create the search box and link it to the UI element.
+let input = document.getElementById('pac-input');
+let searchBox = new google.maps.places.SearchBox(input);
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+// Bias the SearchBox results towards current map's viewport.
+map.addListener('bounds_changed', function() {
+  searchBox.setBounds(map.getBounds());
+});
+
+let markers2 = [];
+// Listen for the event fired when the user selects a prediction and retrieve
+// more details for that place.
+searchBox.addListener('places_changed', function() {
+  let places = searchBox.getPlaces();
+
+  if (places.length == 0) {
+    return;
+  }
+
+  // Clear out the old markers.
+  markers2.forEach(function(marker) {
+    marker.setMap(null);
+  });
+  markers2 = [];
+
+  // For each place, get the icon, name and location.
+  let bounds = new google.maps.LatLngBounds();
+  places.forEach(function(place) {
+    if (!place.geometry) {
+      console.log("Returned place contains no geometry");
+      return;
+    }
+    let icon = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    // Create a marker for each place.
+    markers.push(new google.maps.Marker({
+      map: map,
+      icon: icon,
+      title: place.name,
+      position: place.geometry.location
+    }));
+
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  });
+  map.fitBounds(bounds);
+});
+
+} // END INITMAP
+
+/** @this {google.maps.Polygon} */
+function showArrays(event) {
+    
+    let contentString = '<h3>Shopping Area</h3><p>This area encompasses O\' Connell Street and pedestrianised Henry Street areas.</p>';
+    // Replace the info window's content and position.
+    infoWindow.setContent(contentString);
+    infoWindow.setPosition(event.latLng);
+
+    infoWindow.open(map);
+  }
 
 /** @description toggles the type of map category
  * 
@@ -615,12 +693,12 @@ function toggleType(elm, event, type) {
     }
 }
 
-function addLine() {
+function addPoly() {
     // set the map to put polyline on
     shoppingAreaNorth.setMap(map);
 }
 
-function removeLine() {
+function removePoly() {
     shoppingAreaNorth.setMap(null);
 }
 function whereAmI() {
